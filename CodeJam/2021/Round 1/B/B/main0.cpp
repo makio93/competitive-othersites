f0958fc5528ed52d21f0f5711da4a8d0c89b01b0
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2021-04-30 00:57:38 +0900

    CJ2021-R1B-A問の修正、B問のSet1までの復習

--- CodeJam/2021/Round 1/B/B/main0.cpp
+++ CodeJam/2021/Round 1/B/B/main0.cpp
@@ -9,6 +9,8 @@ using ll = long long;
 #define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
 #define all(x) (x).begin(), (x).end()
 
+// 本番中に実装、WA
+
 int main() {
     int t;
     cin >> t;
---
+++ CodeJam/2021/Round 1/B/B/main1a.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+// 復習時、解説を見てから実装、Set 1までの解答、Set2までAC
+
+int main() {
+    int t;
+    cin >> t;
+    rep3(i1, 1, t+1) {
+        int n, a, b;
+        cin >> n >> a >> b;
+        vector<int> u(n);
+        rep(i, n) cin >> u[i];
+        map<int, int> uval;
+        rep(i, n) if (u[i] > 0) uval[i+1] = u[i];
+        int rval = uval.rbegin()->first;
+        while (rval <= (int)(1e3)) {
+            map<int, int> h, d = uval;
+            h[rval] = 1;
+            while (!h.empty() && !d.empty()) {
+                for (auto itr=d.begin(); itr!=d.end(); ++itr) if (h.find(itr->first) != h.end()) {
+                    int sub = min(h[itr->first], itr->second);
+                    h[itr->first] -= sub;
+                    itr->second -= sub;
+                    if (h[itr->first] <= 0) h.erase(itr->first);
+                }
+                for (auto itr=d.begin(); itr!=d.end(); ) {
+                    if (itr->second == 0) itr = d.erase(itr);
+                    else ++itr;
+                }
+                if (!h.empty()) {
+                    int mval = h.rbegin()->first, mcnt = h.rbegin()->second;
+                    if (mval-a >= 1) h[mval-a] = h[mval-a] + mcnt;
+                    if (mval-b >= 1) h[mval-b] = h[mval-b] + mcnt;
+                    h.erase(mval);
+                }
+            }
+            if (d.empty()) break;
+            ++rval;
+        }
+        if (rval <= (int)(1e3)) cout << "Case #" << i1 << ": " << rval << endl;
+        else cout << "Case #" << i1 << ": " << "IMPOSSIBLE" << endl;
+    }
+    return 0;
+}
