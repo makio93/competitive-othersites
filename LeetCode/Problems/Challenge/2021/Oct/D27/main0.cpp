a29d98c583d9b78b63426e71d62b89e3f3595a1d
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2021-10-27 18:09:36 +0900

    LeetCode-DC2021Oct27-自力AC

---
+++ LeetCode/Problems/Challenge/2021/Oct/D27/main0.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 自力AC
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int lid = 0, rid = n-1;
+        while (lid<n && nums[lid]==0) ++lid;
+        while (rid>=0 && nums[rid]==2) --rid;
+        for (int i=lid; i<=rid; ++i) {
+            if (nums[i] == 0) { swap(nums[lid], nums[i]); ++lid; }
+            else if (nums[i] == 2) { swap(nums[rid], nums[i]); --rid; --i; }
+        }
+    }
+};
