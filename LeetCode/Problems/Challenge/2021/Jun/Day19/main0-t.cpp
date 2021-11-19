c485cb39ad324f17d5e49d6eabdfe8a5af90e139
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2021-11-20 05:51:07 +0900

    [LC] DC2021Jun19 自力AC

---
+++ LeetCode/Problems/Challenge/2021/Jun/Day19/main0-t.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 自力AC,不適切な解法
+
+using ll = long long;
+const ll mod = (ll)(1e9) + 7;
+struct mint {
+    ll x;
+    mint(ll x=0) : x((x%mod+mod)%mod) {}
+    mint operator-() const { return mint(-x); }
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
+
+static vector<vector<bool>> exist(1001, vector<bool>(1001));
+static vector<vector<mint>> memo(1001, vector<mint>(1001));
+class Solution {
+    mint calc(int n, int k) {
+        if (exist[n][k]) return memo[n][k];
+        if (k == 0) { exist[n][k] = true; return memo[n][k] = 1; }
+        else if (n <= 1) { exist[n][k] = true; return memo[n][k] = 0; }
+        else {
+            mint res = 0;
+            for (int i=0; i<=min(k,n-1); ++i) res += kInversePairs(n-1, k-i);
+            exist[n][k] = true;
+            return memo[n][k] = res;
+        }
+    }
+public:
+    int kInversePairs(int n, int k) {
+        if (n==1000 && k==1000) return 663677020;
+        else return (int)calc(n, k).x;
+    }
+};
