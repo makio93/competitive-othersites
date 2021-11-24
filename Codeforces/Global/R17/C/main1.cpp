cc6838e3736f4ca1906a81bc7e9bebd3e8a02151
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2021-11-25 02:09:17 +0900

    [CF] Global R17-C 解説AC

---
+++ Codeforces/Global/R17/C/main1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+// 解説AC
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t0;
+	cin >> t0;
+	rep(i0, t0) {
+		int n;
+		cin >> n;
+		vector<int> a(n), b(n);
+		rep(i, n) cin >> a[i] >> b[i];
+		int l = 1, r = n + 1;
+		while (r-l > 1) {
+			int c = l + (r-l) / 2;
+			int cnt = 0;
+			rep(i, n) if ((cnt>=c-1-a[i]) && (cnt<=b[i])) ++cnt;
+			if (cnt >= c) l = c;
+			else r = c;
+		}
+		cout << l << endl;
+	}
+	return 0;
+}
