47141e2b16738cbffab1cdc84fb9c493558a743e
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-02-16 15:16:04 +0900

    [LC] DCC2022Feb16自力AC

---
+++ LeetCode/Problems/Challenge/2022/Feb/W3/D16/main0.cpp
@@ -0,0 +1,26 @@
+// 自力AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head!=nullptr && head->next!=nullptr) {
+            auto nptr = head->next;
+            head->next = swapPairs(head->next->next);
+            nptr->next = head;
+            head = nptr;
+        }
+        return head;
+    }
+};
