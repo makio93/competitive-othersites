39b8ff375daf5c8ca3b9d9ca4780ab959f765e54
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-06-20 18:39:40 +0900

    [LC] Weekly296-Q3:復習:解説AC追加

---
+++ LeetCode/Contest/Weekly/296/Q3/main1-2.cpp
@@ -0,0 +1,18 @@
+// 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static vector<int> vids((int)(1e6)+1, -1);
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+        int n = nums.size(), m = operations.size();
+        for (int i=0; i<n; ++i) vids[nums[i]] = i;
+        for (int i=0; i<m; ++i) {
+            nums[vids[operations[i][0]]] = operations[i][1];
+            vids[operations[i][1]] = vids[operations[i][0]];
+        }
+        return nums;
+    }
+};
---
+++ LeetCode/Contest/Weekly/296/Q3/main1.cpp
@@ -0,0 +1,18 @@
+// 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+        int n = nums.size(), m = operations.size();
+        unordered_map<int, int> vids;
+        for (int i=0; i<n; ++i) vids[nums[i]] = i;
+        for (int i=0; i<m; ++i) {
+            nums[vids[operations[i][0]]] = operations[i][1];
+            vids[operations[i][1]] = vids[operations[i][0]];
+        }
+        return nums;
+    }
+};
