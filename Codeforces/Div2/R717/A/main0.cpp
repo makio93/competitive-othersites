9176b7ffa3cff7665596b707f9c9a9daa1035476
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-07-20 01:12:46 +0900

    [CF] R717(Div.2):ファイルの移動+コメント追加

---
+++ Codeforces/Div2/R717/A/main0.cpp
@@ -0,0 +1,32 @@
+// 本番AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+	int t;
+	cin >> t;
+	rep(i1, t) {
+		int n, k;
+		cin >> n >> k;
+		vector<int> a(n);
+		rep(i, n) cin >> a[i];
+		rep(i, n) {
+			int sub = min(k, a[i]);
+			a[i] -= sub;
+			k -= sub;
+			a[n-1] += sub;
+			if (k <= 0) break;
+		}
+		rep(i, n) printf("%d%c", a[i], (i<n-1?' ':'\n'));
+	}
+	return 0;
+}
