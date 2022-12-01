d76ebe33249cfe629003d448dab41f6c9ce9a610
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-12-02 00:15:28 +0900

    [yuki] yukicoder:各ファイル構成の整理

---
+++ yukicoder/other/Calendar/2022/A/main.cpp
@@ -0,0 +1,21 @@
+// 
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+
+	return 0;
+}
