a3c4a78a6854bf0f651b5bc13181dc4b93c98f58
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2023-05-12 01:43:37 +0900

    [LC] JSC30D-T3-D07:初回学習分

---
+++ LeetCode/Problems/JSC30D/T3/D07/main1-2.js
@@ -0,0 +1,16 @@
+// 解説AC2
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+	return function(x) {
+        return functions.reduceRight((val, func) => func(val), x);
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
