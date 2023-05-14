f356db0f12be7f61b827d3d7d8321d16f232250a
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2023-05-14 16:19:40 +0900

    [LC] JSC30D-T3-D08:初回学習分

---
+++ LeetCode/Problems/JSC30D/T3/D08/main1-3.js
@@ -0,0 +1,23 @@
+// 解説AC3
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    var called = false;
+    return function(...args){
+        if (!called) {
+            called = true;
+            return fn.apply(this, args);
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
