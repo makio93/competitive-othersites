2588eff963f026b9776267b6c3e218d2787d1ebc
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2023-06-03 11:48:04 +0900

    [CF] R822(Div.2)-A:学習1回目:自力AC

---
+++ Codeforces/Div2/R822/A/1/main0.cpp
@@ -0,0 +1,32 @@
+// 学習1回目,自力AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(2e9);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t0;
+    cin >> t0;
+    rep(i0, t0) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        rep(i, n) cin >> a[i];
+        sort(all(a));
+        int res = INF;
+        rep3(i, 1, n-1) res = min(res, a[i+1]-a[i-1]);
+        cout << res << endl;
+    }
+    return 0;
+}
