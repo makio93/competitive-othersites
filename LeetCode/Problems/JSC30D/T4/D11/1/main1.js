71f7a35fc34511f59b0e962aa27924d165cf3c1e
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2023-06-19 17:15:35 +0900

    [LC] JSC30d-T4-D11:学習1回目:解説AC

---
+++ LeetCode/Problems/JSC30D/T4/D11/1/main1.js
@@ -0,0 +1,15 @@
+// 学習1回目,解説AC
+
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    return new Promise((res) => {
+        setTimeout(res, millis);
+    });
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
