01bfd5a5009fb954f3dd0ad6183d0e0170d48293
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2023-08-02 08:23:38 +0900

    [LC] 30DoJS-T1-D01:初回学習分

---
+++ LeetCode/StudyPlan/30DoJS/T1/D01/main0-1.js
@@ -0,0 +1,13 @@
+// 自力AC1
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return (...args) => "Hello World";
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
---
+++ LeetCode/StudyPlan/30DoJS/T1/D01/main0-2.js
@@ -0,0 +1,11 @@
+// 自力AC2
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = () => (...args) => "Hello World";
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
